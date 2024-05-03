46374b0036f745a9b8243bce8cc3b11ca6ec75f7
Vini Duarte <85229756+vinizika@users.noreply.github.com>
2024-05-03 16:51:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+from math import * 
 
 def menu():
     print("Simulador Crocodilo de Perdizes\n")
@@ -12,7 +14,19 @@ while(True):
     if n == 1:
         print("Opcao [1] selecionada")
     elif n == 2:
-        print("Opcao [2] selecionada")
+        A = float(input("Digite o valor da amplitude (A): "))
+        k = float(input("Digite o valor do número de onda (k): "))
+        Xp = float(input("Digite a posição específica (Xp) onde deseja calcular a probabilidade: "))
+
+        L = 2/A**2
+        n = round(k * L / np.pi)  
+
+        probabilidade_xp = L*Xp
+        probabilidade = A**2 * np.sin(k * probabilidade_xp)**2
+
+        print(f"Largura da caixa (L): {L} metros")
+        print(f"Nível quântico da partícula (n): {n}")
+        print(f"Probabilidade de encontrar a partícula na posição {probabilidade_xp} é: {probabilidade:.5f}")
     elif n == 3:
         break
     else:
